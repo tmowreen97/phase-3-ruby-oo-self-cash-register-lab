90fbddb442d4b4b0db86d1521a9bb76fad7ef3dd
Tasreen Mowreen <tmowreen97@gmail.com>
2022-10-26 21:12:10 -0400

    commit tree 1f9c643f8fb45f728416cc4ce1932b40bc7a1333

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,25 @@
+require 'pry'
+class CashRegister
+  attr_accessor :discount, :total
 
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+    @last_item_price = 0
+  end
+
+  def total= total
+    @total = total
+  end
+
+  def add_item title, price, quantity = 1
+    # binding.pry
+    @quantity = quantity
+    @total = @quantity*price
+    # binding.pry
+  end
+
+end
+
+c1 = CashRegister.new
